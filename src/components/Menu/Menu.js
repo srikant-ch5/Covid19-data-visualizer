ee6e6f7ea0be81841283788b45c7079943af3e3d
sriks123 <srksriks123@gmail.com>
2020-12-27 02:48:53 +0530

    include git page url commit

--- src/components/Menu/Menu.js
+++ src/components/Menu/Menu.js
@@ -1,64 +1,69 @@
-import React, {useState, useEffect} from "react";
-import {withRouter} from 'react-router-dom';
+import React, { useState, useEffect } from "react";
+import { withRouter } from "react-router-dom";
 import DarkModeToggle from "react-dark-mode-toggle";
-import './Menu.css';
+import "./Menu.css";
 
-import coronaImage from '../../images/corona.jpg'
+import coronaImage from "../../images/corona.jpg";
 
 const Menu = (props) => {
-    const [openMenu, setOpenMenu] = useState(false);
-    const [optionClicked, setOptionClicked] = useState(false);
-    const [isDarkMode, setIsDarkMode] = useState(() => false);  
-       
-    const setClassNames = num => {
-        const classArr = ["m-item"];
-        if(openMenu) classArr.push(`open-${num}`);
- 
-        return classArr.join(' ')
-    }
+  const [openMenu, setOpenMenu] = useState(false);
+  const [optionClicked, setOptionClicked] = useState(false);
+  const [isDarkMode, setIsDarkMode] = useState(() => false);
 
-    
-    useEffect(()=> {
-        const currentRoute= window.location.pathname
-        currentRoute==="/" ? setOptionClicked(false) : setOptionClicked(true);
-    },[])
+  const setClassNames = (num) => {
+    const classArr = ["m-item"];
+    if (openMenu) classArr.push(`open-${num}`);
 
-    const pushToRoute = (routeTopush) => {
-        props.history.push(routeTopush);
-        setOpenMenu(false);
+    return classArr.join(" ");
+  };
 
-        routeTopush==="/"? setOptionClicked(false) : setOptionClicked(true)
-        
-    }
+  useEffect(() => {
+    const currentRoute = window.location.pathname;
+    currentRoute === "/" || currentRoute === "/Covid19-data-visualizer"
+      ? setOptionClicked(false)
+      : setOptionClicked(true);
+  }, []);
 
-    return (
-        <div className={optionClicked ? "MenuRight": "Menu"}>
-            <div className={"m-item m-logo"}
-                onClick={() => setOpenMenu(!openMenu)}>
-                <img style={{width: "50px",
-                    marginTop: "5px"}} alt="COVID-19" src={coronaImage} />
-            </div>
-            <div className={setClassNames(1)}
-                onClick={() => pushToRoute("/IndiaStats")}>
-                India Stats
-            </div>
-            <div className={setClassNames(2)}
-                onClick={() => pushToRoute("/GlobalStats")}>
-                Global Stats
-            </div>
-            <div className={setClassNames(3)}
-                onClick={() => pushToRoute("/")}>
-                Sign out
-            </div>
-            <div className={setClassNames(4)}>
-                <DarkModeToggle
-                    onChange={setIsDarkMode}
-                    checked={isDarkMode}
-                    size={80}
-                    />
-            </div>
-        </div>
-    );
-}
+  const pushToRoute = (routeTopush) => {
+    props.history.push(routeTopush);
+    setOpenMenu(false);
+
+    routeTopush === "/" ? setOptionClicked(false) : setOptionClicked(true);
+  };
+
+  return (
+    <div className={optionClicked ? "MenuRight" : "Menu"}>
+      <div className={"m-item m-logo"} onClick={() => setOpenMenu(!openMenu)}>
+        <img
+          style={{ width: "50px", marginTop: "5px" }}
+          alt="COVID-19"
+          src={coronaImage}
+        />
+      </div>
+      <div
+        className={setClassNames(1)}
+        onClick={() => pushToRoute("/IndiaStats")}
+      >
+        India Stats
+      </div>
+      <div
+        className={setClassNames(2)}
+        onClick={() => pushToRoute("/GlobalStats")}
+      >
+        Global Stats
+      </div>
+      <div className={setClassNames(3)} onClick={() => pushToRoute("/")}>
+        Sign out
+      </div>
+      <div className={setClassNames(4)}>
+        <DarkModeToggle
+          onChange={setIsDarkMode}
+          checked={isDarkMode}
+          size={80}
+        />
+      </div>
+    </div>
+  );
+};
 
 export default withRouter(Menu);
